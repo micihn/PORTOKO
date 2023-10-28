5e9c7ac8dd7d6271d4a21e9c412c7aeee276f4a9
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-10-28 13:10:12 +0800

    [ADD] Multi-sequence feature for Order Pengiriman module to support multi-company via post_init_hook()

--- custom-modul/Perusahaan Angkutan/order_pengiriman/__manifest__.py
+++ custom-modul/Perusahaan Angkutan/order_pengiriman/__manifest__.py
@@ -28,7 +28,7 @@
         'views/oper_order.xml',
         'views/uang_jalan.xml',
         'views/menu.xml',
-        'views/sequences.xml',
+        # 'views/sequences.xml',
     ],
     'demo': [],
     'post_init_hook': 'post_init_hook',
