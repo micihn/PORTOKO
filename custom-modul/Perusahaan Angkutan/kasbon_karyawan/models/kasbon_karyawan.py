ec1bc42f69c854bcc5c8c78fcdae155ab749bb40
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-11-12 20:25:19 +0800

    [ADD] Add Process button to submit input

--- custom-modul/Perusahaan Angkutan/kasbon_karyawan/models/kasbon_karyawan.py
+++ custom-modul/Perusahaan Angkutan/kasbon_karyawan/models/kasbon_karyawan.py
@@ -19,6 +19,7 @@ class KasbonKaryawan(models.Model):
 
 
     active = fields.Boolean('Archive', default=True, tracking=True)
+    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company)
     nama_karyawan = fields.Many2one('res.partner', 'Nama Karyawan')
     rekening_karyawan = fields.Many2one('res.partner.bank', 'Rekening')
     sisa_piutang = fields.Float('Sisa Piutang', compute='_compute_sisa_hutang', digits=(6, 0))
@@ -48,27 +49,50 @@ class KasbonKaryawan(models.Model):
     @api.depends('history_hutang', 'history_bayar')
     def _compute_sisa_hutang(self):
         for record in self:
-            nominal_hutang = sum(record.history_hutang.mapped('nominal_hutang'))
+            nominal_pinjam = sum(record.history_hutang.mapped('nominal_pinjam'))
             nominal_bayar = sum(record.history_bayar.mapped('nominal_bayar'))
 
-            record.sisa_piutang = nominal_hutang - nominal_bayar
+            record.sisa_piutang = nominal_pinjam - nominal_bayar
 
+    def proses(self):
+        if self.tipe_kasbon == 'pinjam':
+            self.env['history.hutang'].create({
+                "kasbon_karyawan": self.id,
+                "company_id": self.env.company.id,
+                "tanggal_hutang": fields.Datetime.now(),
+                "nominal_pinjam": self.nominal_pinjam,
+            })
 
+            self.tipe_kasbon = 'pinjam'
+            self.nominal_pinjam = 0
+
+        elif self.tipe_kasbon == 'bayar':
+            self.env['history.bayar'].create({
+                "kasbon_karyawan": self.id,
+                "company_id": self.env.company.id,
+                "tanggal_bayar": fields.Datetime.now(),
+                "nominal_bayar": self.nominal_bayar,
+            })
+
+            self.tipe_kasbon = 'pinjam'
+            self.nominal_bayar = 0
 
 class HistoryHutang(models.Model):
     _name = 'history.hutang'
     _description = 'History Hutang'
 
     kasbon_karyawan = fields.Many2one('kasbon.karyawan', invisible=True)
+    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company)
     kode_nomor_hutang = fields.Char('No.')
     tanggal_hutang = fields.Date('Tanggal')
-    nominal_hutang = fields.Float('Nominal', digits=(6, 0))
+    nominal_pinjam = fields.Float('Nominal', digits=(6, 0))
 
 class HistoryBayar(models.Model):
     _name = 'history.bayar'
     _description = 'History Bayar'
 
     kasbon_karyawan = fields.Many2one('kasbon.karyawan', invisible=True)
+    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company)
     kode_nomor_bayar = fields.Char('No.')
     tanggal_bayar = fields.Date('Tanggal')
     nominal_bayar = fields.Float('Nominal', digits=(6, 0))
