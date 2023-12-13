1da73084f80b44000dcdaf1faf8dda5ae0327fda
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2023-12-13 11:28:48 +0800

    [FIX] Cost to follow product and product_uom_qty fix

--- custom-modul/Perusahaan Angkutan/fleet_reporting/models/fleet_vehicle_log_services.py
+++ custom-modul/Perusahaan Angkutan/fleet_reporting/models/fleet_vehicle_log_services.py
@@ -13,10 +13,10 @@ class FleetVehicleLogServiceProduct(models.Model):
         return result
 
     name = fields.Char(readonly=True, required=True, copy=False, default='New')
-    product_id = fields.Many2one('product.product')
+    product_id = fields.Many2one('product.product', copy=False)
     company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company)
     product_qty = fields.Float('Quantity', copy=False, default=1)
-    total_amount = fields.Monetary(compute='_compute_total_amount', store=True)
+    total_amount = fields.Monetary(readonly=True, store=True, copy=False, default=0)
     is_service = fields.Boolean()
     amount = fields.Monetary(copy=False)
     initial = fields.Boolean(default=True)
@@ -30,7 +30,7 @@ class FleetVehicleLogServiceProduct(models.Model):
         ('batal', 'Batal'),
     ], default='draft', string='Stage', group_expand='_expand_states', copy=False)
 
-    @api.onchange('service_type_id')
+    @api.onchange('service_type_id', 'product_id')
     def check_service_type_id_value(self):
         if self.service_type_id.category == 'service':
             self.is_service = True
@@ -38,12 +38,13 @@ class FleetVehicleLogServiceProduct(models.Model):
             self.product_qty = 1
             self.amount = 0
             self.total_amount = 0
+            self.product_id = None
 
         elif self.service_type_id.category == 'sparepart':
             self.is_service = False
             self.initial = False
             self.product_qty = 1
-            self.amount = 0
+            self.amount = self.product_id.product_tmpl_id.standard_price
             self.total_amount = 0
 
         elif self.service_type_id.category == 'contract':
@@ -52,15 +53,16 @@ class FleetVehicleLogServiceProduct(models.Model):
             self.product_qty = 1
             self.amount = 0
             self.total_amount = 0
+            self.product_id = None
 
-    @api.depends('product_qty', 'amount')
+    @api.onchange('product_qty', 'amount', 'product_id')
     def _compute_total_amount(self):
         for record in self:
             record.total_amount = record.product_qty * record.amount
 
     def validate(self):
-        account_settings = self.env['fleet.configuration.service'].search([('company_id', '=', self.company_id.id)])
-        if bool(account_settings.operation_type) == False:
+        fleet_settings = self.env['fleet.configuration.service'].search([('company_id', '=', self.company_id.id)])
+        if bool(fleet_settings.operation_type) == False:
             raise ValidationError('Anda belum melakukan pengaturan pada Fleet > Configuration > Service > Configuration')
 
         if self.is_service:
@@ -92,20 +94,20 @@ class FleetVehicleLogServiceProduct(models.Model):
 
         else:
             picking = self.env['stock.picking'].create({
-                'location_id': account_settings.operation_type.default_location_src_id.id,
-                'location_dest_id': account_settings.operation_type.default_location_dest_id.id,
-                'picking_type_id': account_settings.operation_type.id,
+                'location_id': fleet_settings.operation_type.default_location_src_id.id,
+                'location_dest_id': fleet_settings.operation_type.default_location_dest_id.id,
+                'picking_type_id': fleet_settings.operation_type.id,
                 'origin': self.name
             })
             stock_move = self.env['stock.move'].create({
                 'name': self.name + str(' - ' + self.description),
                 'product_id': self.product_id.id,
-                # 'quantity_done': self.product_qty,
+                'product_uom_qty': self.product_qty,
                 'product_uom': self.product_id.product_tmpl_id.uom_id.id,
                 'picking_id': picking.id,
-                'picking_type_id': account_settings.operation_type.id,
-                'location_id': account_settings.operation_type.default_location_src_id.id,
-                'location_dest_id': account_settings.operation_type.default_location_dest_id.id,
+                'picking_type_id': fleet_settings.operation_type.id,
+                'location_id': fleet_settings.operation_type.default_location_src_id.id,
+                'location_dest_id': fleet_settings.operation_type.default_location_dest_id.id,
             })
             stock_move._action_confirm()
             stock_move._action_assign()
--- custom-modul/Perusahaan Angkutan/fleet_reporting/models/stock_picking.py
+++ custom-modul/Perusahaan Angkutan/fleet_reporting/models/stock_picking.py
@@ -17,12 +17,46 @@ class InternalTransferFleet(models.Model):
             if fleet.state != 'done':
                 fleet.action_cancel()
 
+        # if self.fleet_layer == 2:
+        #     fleet_settings = self.env['fleet.configuration.service'].search([('company_id', '=', self.company_id.id)])
+        #     source_location = fleet_settings.operation_type.default_location_src_id.id
+        #     destination_location = fleet_settings.operation_type.default_location_dest_id.id
+        #
+        #     # picking_permintaan = self.env['stock.picking'].search([('origin', '=', self.origin)])
+        #     # picking_permintaan.create_returns()
+        #
+        #     return_picking = self.env['stock.picking'].create({
+        #         'picking_type_id': self.picking_type_id.id,
+        #         'location_id': self.location_dest_id.id,
+        #         'location_dest_id': self.location_id.id,
+        #         'picking_type_id': fleet_settings.operation_type.return_picking_type_id.id,
+        #         'origin': 'Return Of' + self.name
+        #     })
+        #
+        #     for move in self.move_ids_without_package:
+        #         return_move = self.env['stock.move'].create({
+        #             'name': "Return Of" + str(self.fleet_service_id.name),
+        #             'product_id': move.product_id.id,
+        #             'product_uom_qty': move.product_qty,
+        #             'product_uom': move.product_uom.id,
+        #             'picking_id': return_picking.id,
+        #             'picking_type_id': fleet_settings.operation_type.return_picking_type_id.id,
+        #             'location_id': move.location_dest_id.id,
+        #             'location_dest_id': move.location_id.id,
+        #         })
+        #
+        #     return_picking.action_confirm()
+        #     return_picking.action_assign()
+        #     return_picking.button_validate()
+
         return res
 
     def button_validate(self):
         res = super(InternalTransferFleet, self).button_validate()
         if self.fleet_layer == 2:
             self.fleet_service_id.state_record = 'selesai'
-        else:
+        elif self.fleet_layer == 1:
             self.fleet_service_id.state_record = 'diminta'
+        else:
+            pass
         return res
