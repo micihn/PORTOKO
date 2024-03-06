8a8378c26cfc157413c923351512ec5c52f2d583
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2024-03-06 22:59:26 +0800

    [FIX] Variable name

--- custom-modul/Perusahaan Angkutan/report_sparepart/wizard/report_stock_sparepart.py
+++ custom-modul/Perusahaan Angkutan/report_sparepart/wizard/report_stock_sparepart.py
@@ -5,33 +5,33 @@ class ReportStockSparepart(models.TransientModel):
     _name = 'report.stock.sparepart'
     _description = 'Stock Sparepart Wizard'
 
-    tanggal = fields.Datetime()
+    date = fields.Datetime()
 
     def generate_report_sparepart(self):
         products = self.env['product.product'].search([('detailed_type','=','product')])
 
         product_list = []
         for product in products:
-            product_value = 0
-            product_valuation = self.env['stock.valuation.layer'].search([
+            product_value = 0.00
+            product_valuations = self.env['stock.valuation.layer'].search([
                 ('product_id', '=', product.id),
-                ('create_date', '<=', self.tanggal)])
-            for product_val in product_valuation:
-                product_value += product_val.value
+                ('create_date', '<=', self.date)
+            ])
+
+            for product_valuation in product_valuations:
+                product_value += product_valuation.value
 
             product_dict = {
                 'product_code': product.default_code,
                 'product_name': product.name,
                 'product_price': float(product.list_price),
-                'product_qty': float(product.with_context({'to_date': self.tanggal}).qty_available),
+                'product_qty': float(product.with_context({'to_date': self.date}).qty_available),
                 'product_value': float(product_value),
             }
             product_list.append(product_dict)
 
-            # print(product.with_context({'to_date': self.tanggal}).qty_available)
-
-        data = {'tanggal': self.tanggal.strftime('%d/%m/%Y'),
+        data = {'date': self.date.strftime('%d/%m/%Y'),
                 'product_list': product_list,
                 }
 
-        return self.env.ref('report_sparepart.report_fleet_pendapatan_action').report_action([], data=data)
+        return self.env.ref('report_sparepart.report_stock_sparepart_action').report_action([], data=data)
