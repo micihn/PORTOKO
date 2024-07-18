71f4a3bc73404c8c35d82c7d287f5336aed18c50
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2024-07-18 14:59:13 +0800

    [FIX] Hide Order Pengiriman

--- custom-modul/Perusahaan Angkutan/order_pengiriman/__manifest__.py
+++ custom-modul/Perusahaan Angkutan/order_pengiriman/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Order Pengiriman',
+    'name': 'Uang Jalan',
     'author': 'Altela Software',
     'version': '0.2',
     'summary': 'Pencatatan serta tracking pengiriman',
