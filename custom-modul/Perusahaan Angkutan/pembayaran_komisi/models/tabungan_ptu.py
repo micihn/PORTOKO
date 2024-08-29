62a31cd6935d17108dcc9de86c49c2139dfa516c
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2024-08-29 23:45:09 +0800

    [ADD] Tabungan PTU

--- custom-modul/Perusahaan Angkutan/pembayaran_komisi/models/tabungan_ptu.py
+++ custom-modul/Perusahaan Angkutan/pembayaran_komisi/models/tabungan_ptu.py
@@ -11,6 +11,11 @@ class TabunganPTU(models.Model):
     saldo = fields.Float(string="Saldo", compute="compute_saldo_tabungan")
     nominal_ptu = fields.Float(string="Nominal PTU")
 
+    state = fields.Selection([
+        ('draft', "Draft"),
+        ('paid', "Paid"),
+    ], default='draft', string="State")
+
     @api.depends('karyawan')
     def compute_saldo_tabungan(self):
         for record in self:
@@ -19,3 +24,20 @@ class TabunganPTU(models.Model):
                 record.saldo = sum(tabungan.mapped('nominal'))
             else:
                 record.saldo = 0.0
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super(TabunganPTU, self).create(vals_list)
+        for record in records:
+            record.kode = self.env['ir.sequence'].next_by_code('tabungan.ptu.sequence') or 'New'
+        return records
+
+    def create_ptu(self):
+        self.env['hr.employee.ptu_line'].create({
+            'employee_id': self.karyawan.id,
+            'tipe': 'pemasukan',
+            'nominal': self.nominal_ptu,
+            'state': 'diproses',
+        })
+
+        self.state = 'paid'
